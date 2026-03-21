7f4f5d24dddf3ecdf98ea1a932ae49cff7a40bb0
Sabnish776 <sabnish1776@gmail.com>
2026-03-22 00:49:05 +0530

    completed user registration logic

--- backend/src/main/java/com/app/ccms/controller/AuthController.java
+++ backend/src/main/java/com/app/ccms/controller/AuthController.java
@@ -1,3 +1,40 @@
 package com.app.ccms.controller;
 
-public class AuthController {}
+import com.app.ccms.DTO.LoginDTO;
+import com.app.ccms.DTO.RegisterDTO;
+
+import com.app.ccms.model.User;
+import com.app.ccms.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, Object>> register(@RequestBody RegisterDTO registerDTO) {
+        try {
+            userService.register(registerDTO);
+            return ResponseEntity.ok(Map.of("message", "User registered successfully"));
+        } catch (RuntimeException e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(Map.of("message", e.getMessage()));
+        }
+    }
+
+}
--- backend/src/main/java/com/app/ccms/service/UserService.java
+++ backend/src/main/java/com/app/ccms/service/UserService.java
@@ -1,6 +1,40 @@
 package com.app.ccms.service;
+import com.app.ccms.DTO.RegisterDTO;
+import com.app.ccms.model.Role;
+import com.app.ccms.model.User;
+import com.app.ccms.repository.UserRepository;
+import com.app.ccms.utilities.JwtUtil;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+
+    UserService(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil , UserRepository userRepository) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+    }
+
+    public void register(RegisterDTO registerDTO) {
+
+        if (userRepository.findByEmail(registerDTO.getEmail()) != null) {
+            throw new RuntimeException("Email already registered");
+        }
+        User user = new User();
+        user.setName(registerDTO.getName());
+        user.setEmail(registerDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
+        user.setRole(Role.STUDENT);
+        System.out.println("Registering user: " + user.getEmail());
+        userRepository.save(user);
+    }
 }
